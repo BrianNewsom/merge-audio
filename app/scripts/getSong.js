25d0fc782b09858ea2b72e16ac1541ed7a886c0c
BrianNewsom <brian.newsom@colorado.edu>
2014-12-27 12:03:47 -0700

    Overhaul er model to be based upon tracks and stems

--- app/scripts/getSong.js
+++ app/scripts/getSong.js
@@ -1,46 +1,50 @@
 Parse.initialize("s4ZaCLGhg6RCEoQvnxLgQ6Pks1jaIHwEcEH4vC4y", "BhMJzuLaOFee060SkjVohAA7hWCh0Z9geG7Cs2wl");
 
-var songName = document.URL.split('?song=')[1];
+var trackName = document.URL.split('?track=')[1];
 query = new Parse.Query('Track')
 
-query.equalTo('name', songName);
+query.equalTo('name', trackName);
 query.find({
     success:function(res) {
         console.log(res);
         // If song doesn't exist give empty array for stems
+        var stems =  [];
+        var buffers = {};
         if(res.length == 0){
-            var song = {'stems': []};
+            console.log('no stems in track');
+            Mixer.init(stems);
         } else {
             // Result has matching track
             var song = res[0].attributes;
-            var stems = [];
             var stemIds = song.stems;
-            console.log(stemIds);
+
             // Find related stems
+            // Generate list of queries
+            var queries = [];
             for (var i = 0 ; i < stemIds.length ; i++){
-                var query = new Parse.Query('Stem');
-                query.get(stemIds[i], {
-                    success: function(parseStem) {
-                        console.log('found matching stem');
-                        // TODO: Return whole object and do more than just use url.
-                        var obj = {};
-                        var current = parseStem.attributes;
-                        obj[current.name] = current.url;
-                        console.log(obj);
-                        stems.push(obj);
-                    },
-                    error: function(object, error) {
-                        console.log('no matching stem');
-                    }
-                });
+                var q = new Parse.Query('Stem').get(stemIds[i]);
+                queries.push(q);
             }
 
-            // TODO: Give error message to unsupported browsers as in original.
-            window.AudioContext = window.AudioContext || window.webkitAudioContext;
-            context = new AudioContext();
-            loadBuffers(stems);
-        }
-        Mixer.init(song.stems);
+            // Execute queries asynchronously
+            Parse.Promise.when(queries).then(function(){
+                var len = arguments.length;
+                for (var i = 0 ; i < len ; i++){
+                    // TODO: Return whole object and do more than just use url.
+                    var current = arguments[i].attributes;
+                    buffers[current.name] = current.url;
+                    stems.push(current.name);
+                }
+            }).then(function(){
+                // When done, load buffers and initialize mixer
+                // TODO: Give error message to unsupported browsers as in original.
+                window.AudioContext = window.AudioContext || window.webkitAudioContext;
+                context = new AudioContext();
+
+                loadBuffers(buffers);
+                Mixer.init(stems);
+            })
+       }
     },
     error: function(error) {
         // error is an instance of Parse.Error with details about the error.

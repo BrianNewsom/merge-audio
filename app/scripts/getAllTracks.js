c4b3a2d3db259d52c30aaca42eaeb4570afb2ce0
BrianNewsom <brian.newsom@colorado.edu>
2014-12-28 16:12:22 -0700

    Add track limit

--- app/scripts/getAllTracks.js
+++ app/scripts/getAllTracks.js
@@ -1,6 +1,8 @@
+var TRACK_LIMIT = 5;
 $.getJSON("/getAllTracks", function(data){
     var outStr = '';
-    var len = data.length;
+    // Choose min of track limit and data length
+    var len = data.length > TRACK_LIMIT ? TRACK_LIMIT : data.length;
     for (var i=0 ; i < len ; i++){
         outStr= outStr + '<p><a href="/mixer?track=' + data[i] + '">' + String(data[i]) + '</a></p>';
     }
